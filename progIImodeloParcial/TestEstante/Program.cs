443f5f62a4166efac7abddcd3634b1dfe00ea258
ignacio aurtenechea <ignacio.aurtenechea@gmail.com>
2016-11-23 02:44:15 -0300

    acomodando todo este quilombo

--- progIImodeloParcial/TestEstante/Program.cs
+++ progIImodeloParcial/TestEstante/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using progIImodeloParcial;
+using progIImodeloParcial; //incluye todo lo de este namespace dentro del namespace TestPrimerParcial o algo asi.
 
 namespace TestPrimerParcial
 {
